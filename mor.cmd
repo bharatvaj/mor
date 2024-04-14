57ceacf508d86d134b2e7f349bd49cccdba6ffa9
Bharatvaj Hemanth <bharatvaj@yahoo.com>
2024-04-15 02:15:54 +0530

    Add :unzip_archive feature
    Update README, Add Config File section
    
    Support '=' and '&' in the value field of key=value
    
    Add CHANGELOG

--- mor.cmd
+++ mor.cmd
@@ -1,21 +1,14 @@
 @echo off
 setlocal EnableDelayedExpansion
-set mor_version=0.1
+set mor_version=0.2
+set root_dir=%cd%\out\
 
 rem default values
 set /a is_logi=0
 set config_file=requirements.ini
 
-call :read_ini requirements.ini
-
-exit /b
-
 if "%~1" == "" goto print_usage
 goto :main
-
-:unarchive <archive> <destination>
-if "%~1x" == ".zip" do unzip "%~1" -d "%~2"
-goto :eof
 
 :logi
 if %is_logi% equ 1 echo %*
@@ -32,39 +25,69 @@ set /a section_count=0
 set current_section=[
 
 rem locally remove env variables starting with '['
-for /f "usebackq delims== tokens=1" %%l in ( `set [` ) do (
-    set %%l=
-    echo before: set %%l=
-) 2>nul
+set [ 2>nul && for /f "usebackq delims== tokens=1" %%l in ( `set [` ) do (
+	set %%l=
+)
 
-for /f "usebackq delims=: tokens=1,*" %%l in ( `findstr /n /v ^; "%1"` ) do (
-	for /f "usebackq delims==] tokens=1,*" %%a in ( '%%m' ) do (
-        echo actual: %%m
-		set key=%%a
+for /f "usebackq delims=: tokens=1,*" %%l in ( `findstr /n /v ^; %~1` ) do (
+	for /f "usebackq delims==] tokens=1,*" %%a in ( '%%~m' ) do (
+		call :logi actual: %%m
+		set key=%%~a
 		if "!key:~0,1!" == "[" (
 			set current_section=%%a
-            set %%a= 
-			echo section set: !current_section!
+			set %%a= 
+			call :logi section set: !current_section!
 		) else (
-            set [precompiled || (
-                echo [precompiled not found, maybe first?
-            )
-            for /f "usebackq delims== tokens=1,*" %%t in (`set !current_section!`) do (
-                set "!current_section!=%%u %%a %%b "
-                echo set "!current_section!=%%u %%a %%b "
-            )
-
+			for /f "usebackq delims== tokens=1,*" %%t in (`set !current_section!`) do (
+				set !current_section!=%%u %%a "%%b" 
+				call :logi set !current_section!=%%u %%a "%%b" 
+			)
 		)
 	)
 )
-for /f "useback delims== tokens=1,*" %%l in (`set [`) do (
-    echo %%l: %%m
+
+for /f "usebackq delims==[ tokens=1,*" %%l in (`set [`) do (
+	call :prime_download %%l %%~m 
 )
-echo --------------
-for /f "tokens=1,2" %%t in ( "!%section%!" ) do (
-	echo %%t
-	echo %%u
+endlocal
+goto :eof
+
+:prime_download
+setlocal EnableDelayedExpansion
+set target=%1
+shift
+for /f %%a in ("%targets%") do (
+	if "!target!" == "%%a" (
+		set current_target_dir="%root_dir%\%%a"
+		call :logi Current Target Dir: !current_target_dir!
+		if not exist "!current_target_dir!" mkdir "!current_target_dir!"
+		:key_value
+		if "%~1" == "" goto :eof
+		for %%i in (%2) do set ext=%%~xi
+		call :download_archive !current_target_dir! %1 %2 !ext!
+		call :unzip_archive !current_target_dir! %1 !ext!
+		shift
+		shift
+		goto :key_value
+	)
 )
+endlocal
+goto :eof
+
+:unzip_archive
+setlocal
+	echo x [%2]
+	tar xzf "%1\%2%3" -C %1
+endlocal
+goto :eof
+
+:download_archive
+setlocal
+	echo v [%2] %3
+	bitsadmin /rawreturn /transfer "mor %2" %3 "%1\%2%4" || (
+		echo fatal: Failed while downloading %3
+		exit /b
+	)
 endlocal
 goto :eof
 
@@ -72,7 +95,7 @@ goto :eof
 setlocal
 :parse
 set arg=%~1
-if "%~1" == "" goto :eof
+if "%~1" == "" goto :main_continue
 if "%~1" == "-v" (
 	echo mor v%mor_version%
 	goto :eof
@@ -102,8 +125,9 @@ if "%arg:~0,1%" == "=" echo "= command"
 
 shift
 goto parse
+:main_continue
+call :read_ini "%config_file%"
 endlocal
 goto :eof
-
 
 endlocal
